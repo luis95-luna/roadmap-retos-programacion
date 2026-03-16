06e6b4263c8d47e18739b36c90ce5cb3f4871d7d
luis95-luna <luis.otto95@gmail.com>
2026-03-16 14:47:41 -0600

    Update luis95-luna.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/luis95-luna.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/luis95-luna.py
@@ -1,1 +1,28 @@
+https://python.org/
 
+# Comentario en una línea
+
+"""
+Esto también es
+un comentario
+en varias líneas
+"""
+
+'''
+Esto también es
+un comentario
+en varias líneas
+'''
+
+my_variable = "Hola soy tu variable"
+my_variable = "Ya no"
+
+MY_CONSTANT = "De aquí no me muevo"
+
+my_integer = 19
+my_float = 1.9
+my_bool = True
+my_other_bool = False
+my_string = "Lucho"
+
+print ("¡Hola, Python!")
